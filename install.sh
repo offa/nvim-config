d06c6a3b881ff4e2dc8154beafe646bd3f53a145
offa <bm-dev@yandex.com>
2022-06-19 21:41:31 +0200

    Update installer (#123)

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
+# nvim-config - NeoVim config
 #
-# Copyright 2016-2022 offa
+# Copyright (C) 2016-2022  offa
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 RC_DIR=${HOME}
 NVIM_CONF_DIR=${RC_DIR}/.config/nvim
@@ -49,7 +50,7 @@ createLink() {
 }
 
 linkConfig() {
-    createLink $PWD/$1 ${NVIM_CONF_DIR}/$1
+    createLink "$PWD/$1" "${NVIM_CONF_DIR}/$1"
 }
 
 installPluginManager() {
@@ -79,13 +80,11 @@ checkProgram curl
 mkdir -p ${NVIM_CONF_DIR}
 
 # Create backup of files if already existing
-backupIfExisting ${RC_DIR}/.vimrc
-backupIfExisting ${NVIM_CONF_DIR}/init.vim
+backupIfExisting ${NVIM_CONF_DIR}/init.lua
 
 
 # Create links
-linkConfig init.vim
-createLink ${NVIM_CONF_DIR}/init.vim ${RC_DIR}/.vimrc
+linkConfig init.lua
 ok "Links created"
 
 
