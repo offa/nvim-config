724c4ee2a52253683ffade5eaca0acbfb725d2f7
offa <bm-dev@yandex.com>
2022-10-17 17:38:30 +0200

    Replace deprecated nvim-cmp call

--- init.lua
+++ init.lua
@@ -184,7 +184,7 @@ cmp.setup.cmdline(":", {
     })
 
 -- Setup LSP
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 require("lspconfig")["clangd"].setup { capabilities = capabilities }
 require("lspconfig")["cmake"].setup { capabilities = capabilities }
 require("lspconfig")["pylsp"].setup { capabilities = capabilities }
