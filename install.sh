446702f2e8075fb45b29c6a863e4b61c80cd577e
offa <8887756+offa@users.noreply.github.com>
2026-01-06 19:41:58 +0000

    Update year (#152)

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # nvim-config - NeoVim config
 #
-# Copyright (C) 2016-2025  offa
+# Copyright (C) 2016-2026  offa
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
