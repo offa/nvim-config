1112d41dc6aa65fefaaff807057365763c596b5b
offa <8887756+offa@users.noreply.github.com>
2026-08-20 18:12:36 +0200

    Replace vim.loop with vim.uv

--- init.lua
+++ init.lua
@@ -63,11 +63,11 @@ opt.viewdir = dataPath .. "/views"
 
 -- Bootstrap lazy.nvim
 local lazypath = dataPath .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
-  })
+if not vim.uv.fs_stat(lazypath) then
+    vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath,
+    })
 end
 opt.rtp:prepend(lazypath)
 
